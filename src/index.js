6443a398d821b8bd292c4b11c2a3b709381c10b5
Alex Carreira <aycarrei@gmail.com>
2020-02-03 00:55:14 -0500

    Fix logging scope preserve and restore along with default level.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { createSidSvcs } from './utils/sidServices.js'
 import { getCloudUser } from './utils/cloudUser.js'
 import * as serviceWorker from './serviceWorker';
 
-const log = require('loglevel')
+import { getLog, setDebugScope, setAllDebugScopes } from './utils/debugScopes.js'
+// Get the log setup here... (even if not used yet).
+// eslint-disable-next-line
+const log = getLog()
 
 
 const SESSION_FROM_LOCAL = 'sessionData';
@@ -39,77 +42,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
-
-// TODO: clean this up after we get it working again (refactor to correct spot etc)
-
-// TODO: Figure out a good way to refactor the consts below and method to a common
-//       file shared with the widget (which uses these from the file debugScopes.js)
-//
-const ROOT_KEY = 'loglevel'
-const ALLOWED_SCOPES = [ ROOT_KEY,
-                        `${ROOT_KEY}:cloudUser`,
-                        `${ROOT_KEY}:dataProcessing`,
-                        `${ROOT_KEY}:sidServices` ]
-const ALLOWED_LEVELS = [ 'TRACE', 'DEBUG', 'INFO', 'WARN', 'ERROR' ]
-const DEFAULT_LOG_LEVEL="INFO"
-
-/**
- *  getDebugScopes:
- *
- *  Fetches known debug scopes from local storage to forward to the widget
- *  iFrame for dynamic debug capability from an App Console.
- *
- *  @returns a map of the scope keys to their string values.
- */
-function getIFrameDebugScopes() {
-  const debugScopes = {}
-  for (const key of ALLOWED_SCOPES) {
-    debugScopes[key] = DEFAULT_LOG_LEVEL
-  }
-
-  for (const scopeKey in debugScopes) {
-    try {
-      const scope = localStorage.getItem(scopeKey)
-      if (ALLOWED_LEVELS.includes(scope)) {
-        debugScopes[scopeKey] = scope
-      }
-    } catch (suppressedError) {
-      log.debug(`Suppressed error fetching ${scopeKey} from local storage. Setting ${scopeKey} to default value, ${DEFAULT_LOG_LEVEL}.\n${suppressedError}`)
-    }
-  }
-
-  return debugScopes
-}
-
-function setDebugScope(scopeKey, scopeLevel) {
-  if (scopeKey && !scopeKey.startsWith(ROOT_KEY)) {
-    scopeKey = `${ROOT_KEY}:${scopeKey}`
-  }
-
-  if (!scopeLevel) {
-    scopeLevel = 'DEBUG'
-  }
-
-  if (ALLOWED_SCOPES.includes(scopeKey) && ALLOWED_LEVELS.includes(scopeLevel)) {
-    localStorage.setItem(scopeKey, scopeLevel)
-  }
-}
-
-function setAllDebugScopes(scopeLevel='DEBUG') {
-  if (!ALLOWED_LEVELS.includes(scopeLevel)) {
-    console.log(`Scope level ${scopeLevel} is not supported.  Supported levels are ${JSON.stringify(ALLOWED_LEVELS, 0, 2)}.`)
-    return
-  }
-
-  const debugScopes = getIFrameDebugScopes()
-  for (const scopeKey in debugScopes) {
-    localStorage.setItem(scopeKey, scopeLevel)
-  }
-
-  return
-}
 
 
 // Workaround for queck and easy debug from browser console
