f83acf153b0fb44dbeb9e8e78569d2574167d6dd
polluterofminds <justin@graphitedocs.com>
2020-03-24 11:52:50 -0500

    chat stuff

--- src/components/Support.js
+++ src/components/Support.js
@@ -3,7 +3,6 @@ import StickyNav from './StickyNav'
 import Table from 'react-bootstrap/Table'
 import Card from 'react-bootstrap/Card'
 import FormControl from 'react-bootstrap/FormControl'
-const Chart = require('chart.js')
 const moment = require('moment')
 export default class Support extends React.Component {
   constructor(props) {
@@ -187,33 +186,7 @@ export default class Support extends React.Component {
     //const { openChatThreads, closedChatThreads } = this.global
     const conversationsByDay = document.getElementById('conversationsByDay')
     if(conversationsByDay) {
-      new Chart(conversationsByDay, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        }
-      })
+     // Reports here eventually
     }
     
     return (
